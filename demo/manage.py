02ca070f8a61bbe543f7e96240771327d57f9552
Nekmo <contacto@nekmo.com>
2016-04-11 05:46:33 +0200

    changed sys.path in demo for Travis.

--- demo/manage.py
+++ demo/manage.py
@@ -2,6 +2,13 @@
 import os
 import sys
 
+try:
+    import djangocms_comments
+except ImportError:
+    sys.path.append(os.path.abspath(os.path.join(os.path.abspath(__file__), '../../')))
+    import djangocms_comments
+
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demo_app.settings")
 
