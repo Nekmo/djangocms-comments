286d106fcd4f1a9921732d19905158c183412099
Nekmo <contacto@nekmo.com>
2016-04-28 05:46:08 +0200

    Gulp js.

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,30 @@
 var gulp = require('gulp'),
     compass = require('gulp-compass');
     cleanCSS = require('gulp-clean-css');
-    var gutil = require('gulp-util');
+    uglify = require('gulp-uglify');
+    concat = require('gulp-concat');
 
-var defaultTasks = [];
 
-// CSS
-var srcs = {
+var SRCS = {
     boostrap3: 'djangocms_comments/boilerplates/bootstrap3/static/djangocms_comments',
     main: 'djangocms_comments/static/djangocms_comments'
 };
 
+JS_FILES = ['src/js/comments.js', 'src/libs/autosize/dist/autosize.js', 'src/libs/blueimp-md5/dist/md5.js'];
+
+var defaultTasks = ['minify-js'];
+
+// CSS
+
 function staticCSS(src){
-    var cwd = srcs[src];
+    var cwd = SRCS[src];
     gulp.task(src + '-sass', function () {
-        gutil.log(cwd);
+
         return gulp.src('src/scss/*.scss', {cwd: cwd})
             .pipe(compass({
                 // project_path: cwd,
                 css: cwd + '/dist/css',
                 sass: cwd + '/src/scss',
-                debug: true,
                 environment: 'production'
             }))
     });
@@ -33,9 +37,19 @@ function staticCSS(src){
     });
 }
 
-for (var key in srcs) {
+for (var key in SRCS) {
     staticCSS(key);
     defaultTasks.push(key + '-minify-css');
 }
+
+gulp.task('minify-js', function () {
+    return gulp.src(JS_FILES, {cwd: SRCS['main']})
+        .pipe(concat('comments.min.js'))
+        .pipe(uglify({
+            // project_path: cwd,
+        }))
+        .pipe(gulp.dest(SRCS['main'] + '/dist/js/'));
+});
+
 
 gulp.task('default', defaultTasks);
