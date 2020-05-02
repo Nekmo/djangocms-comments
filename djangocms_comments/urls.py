0136e9dbcbaafa8d485f08c1c05a82f2ceba5854
Yakov Boyko <mostselfishman@gmail.com>
2020-05-02 05:33:49 -0500

    Deleting unused code.

--- djangocms_comments/urls.py
+++ djangocms_comments/urls.py
@@ -1,13 +1,7 @@
 from django.conf.urls import *
 
 from djangocms_comments.views import SaveComment
-from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     url(r'^ajax/save_comment$', SaveComment.as_view(), name='djangocms_comments_save_comment'),
 ]
-
-# ('',
-#     # url(r'^$', 'main_view', name='app_main'),
-#     url(r'^ajax/save_comment$', SaveComment.as_view(), name='djangocms_comments_save_comment'),
-# )
